1a7743221fa638dd9650c9e35dbdc26d12a757ff
Daniel Broad <daniel@dorada.org>
2016-02-04 16:57:26 +0000

    Windows Switch

--- react-native-winjs/Libraries/Switch/Switch.web.js
+++ react-native-winjs/Libraries/Switch/Switch.web.js
@@ -7,6 +7,7 @@
 'use strict';
 
 import React, { PropTypes } from 'react';
+import ReactWinJS from 'react-winjs';
 
 var Switch = React.createClass({
 
@@ -30,102 +31,73 @@ var Switch = React.createClass({
   getInitialState: function() {
     return {
       value: this.props.value,
-      disabled: this.props.disabled
+      disabled: this.props.disabled,
+      previousValue: this.props.value
     };
   },
 
   componentWillReceiveProps: function(nextProps) {
     this.setState({
       value: nextProps.value,
-      disabled: nextProps.disabled
+      disabled: nextProps.disabled,
+      previousValue: nextProps.value
     });
   },
 
   getStyles: function() {
     return {
       span: {
-        position: 'relative',
-        display: 'inline-block',
-        margin: 2,
-        height: 30,
-        width: 50,
         cursor: 'default', // pointer will cause a grey background color on chrome
-        verticalAlign: 'middle',
-        borderRadius: 20,
-        borderColor: '#dfdfdf',
-        borderWidth: 1,
-        borderStyle: 'solid',
-        WebkitUserSelect: 'none',
-        WebkitBoxSizing: 'content-box',
-        WebkitBackfaceVisibility: 'hidden'
       },
       checkedSpan: {
-        borderColor: this.props.onTintColor,
-        backgroundColor: this.props.onTintColor,
-        boxShadow: this.props.onTintColor + ' 0 0 0 16px inset',
-        WebkitTransition: 'border 0.2s, box-shadow 0.2s, background-color 1s'
+//         borderColor: this.props.onTintColor,
+//         backgroundColor: this.props.onTintColor,
       },
       uncheckedSpan: {
-        borderColor: '#dfdfdf',
-        backgroundColor: this.props.tintColor,
-        boxShadow: '#dfdfdf 0 0 0 0 inset',
-        WebkitTransition: 'border 0.2s, box-shadow 0.2s'
+//        backgroundColor: this.props.tintColor,
       },
       disabledSpan: {
         opacity: 0.5,
         cursor: 'not-allowed',
         boxShadow: 'none'
       },
-      small: {
-        position: 'absolute',
-        top: 0,
-        width: 30,
-        height: 30,
-        backgroundColor: this.props.thumbTintColor,
-        borderRadius: '100%',
-        boxShadow: '0 1px 3px rgba(0,0,0,0.4)',
-        WebkitTransition: '-webkit-transform 0.2s ease-in'
-      },
-      checkedSmall: {
-        WebkitTransform: 'translateX(20px)'
-      },
-      uncheckedSmall: {
-        WebkitTransform: 'translateX(0)'
-      }
     };
   },
 
-  handleClick: function(e) {
+  toggleReference: function(winjstoggle) {
+    this.winjstoggle = winjstoggle;
+  },
+
+  handleChange: function(e) {
     if (this.state.disabled) {
       return null;
     }
 
-    var newVal = !this.state.value;
-    this.props.onValueChange && this.props.onValueChange.call(this, newVal);
-    this.setState({
-      value: newVal
-    });
+//    console.log("value = " + this.winjstoggle.winControl.checked);
+    if (this.state.value !== this.winjstoggle.winControl.checked) {
+      this.props.onValueChange && this.props.onValueChange.call(this, this.winjstoggle.winControl.checked);
+      this.setState({
+        value: this.winjstoggle.winControl.checked,
+      });
+    }
 
-    var oldValue = this.props.value;
-    setTimeout(function() {
-      if (this.props.value == oldValue) {
-        this.setState({
-          value: this.props.value
-        });
-      }
-    }.bind(this), 200);
   },
 
   render: function() {
     var styles = this.getStyles();
     var spancss = this.state.value ? {...styles.span, ...styles.checkedSpan} : {...styles.span, ...styles.uncheckedSpan};
-    var smallcss = this.state.value ? {...styles.small, ...styles.checkedSmall} : {...styles.small, ...styles.uncheckedSmall};
     spancss = this.state.disabled ? {...spancss, ...styles.disabledSpan} : spancss;
 
     return (
-      <span onClick={this.handleClick} style={spancss}>
-        <small style={smallcss}/>
-      </span>
+      <ReactWinJS.ToggleSwitch
+        ref={this.toggleReference}
+        style={spancss}
+        checked={this.state.value}
+        onChange={this.handleChange}
+        disabled={this.state.disabled}
+         labelOn=""
+         labelOff=""
+      />
     );
   }
 
