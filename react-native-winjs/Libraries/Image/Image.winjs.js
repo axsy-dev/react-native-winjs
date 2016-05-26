5b284092e1d85b9f50292523d1ea2c0489a6e464
Gill Peacegood <gill.peacegood@payliquid.com>
2016-05-26 19:40:48 +0100

    add the onLoadStart/onLoadEnd/onLoad callbacks to Image

--- react-native-winjs/Libraries/Image/Image.winjs.js
+++ react-native-winjs/Libraries/Image/Image.winjs.js
@@ -11,6 +11,16 @@ import View from 'ReactView';
 import { Mixin as LayoutMixin } from 'ReactLayoutMixin';
 import ImageResizeMode from './ImageResizeMode';
 
+const STATUS_ERRORED = 'ERRORED'
+const STATUS_LOADED = 'LOADED'
+const STATUS_LOADING = 'LOADING'
+const STATUS_PENDING = 'PENDING'
+const STATUS_IDLE = 'IDLE'
+
+function resolveAssetSource(source) {
+  return ((typeof source === 'object') ? source.uri : source) || null
+}
+
 var Image = React.createClass({
   statics: {
     resizeMode: ImageResizeMode,
@@ -21,7 +31,14 @@ var Image = React.createClass({
   contextTypes: {
     isInAParentText: React.PropTypes.bool
   },
-  render: function() {
+
+  getInitialState() {
+    const uri = resolveAssetSource(this.props.source)
+    // state
+    return { status: uri ? STATUS_PENDING : STATUS_IDLE }
+  },
+
+  render() {
 
     var props = {...this.props};
     props.src = typeof props.source === 'string' ? props.source : props.source.uri;
@@ -44,11 +61,88 @@ var Image = React.createClass({
         </View>
       );
     } else {
+
       return (
         <img {...props}/>
       );
     }
+  },
+
+  _createImageLoader() {
+    const uri = resolveAssetSource(this.props.source)
+
+    this._destroyImageLoader()
+    this.image = new window.Image()
+    this.image.onerror = e => {this._onError(e);}
+    this.image.onload = e => {this._onLoad(e);}
+    this.image.src = uri
+    this._onLoadStart()
+  },
+
+  _destroyImageLoader() {
+   if (this.image) {
+     this.image.onerror = null
+     this.image.onload = null
+     this.image = null
+   }
+ },
+
+ _onError(e) {
+    const { onError } = this.props
+    const event = { nativeEvent: e }
+
+    this._destroyImageLoader()
+    this.setState({ status: STATUS_ERRORED })
+    this._onLoadEnd()
+    if (onError) onError(event)
+  },
+
+  _onLoad(e)  {
+    const { onLoad } = this.props
+    const event = { nativeEvent: e }
+
+    this._destroyImageLoader()
+    this.setState({ status: STATUS_LOADED })
+    if (onLoad) onLoad(event)
+    this._onLoadEnd()
+  },
+
+  _onLoadEnd () {
+     const { onLoadEnd } = this.props
+     if (onLoadEnd) onLoadEnd()
+   },
+
+   _onLoadStart () {
+     const { onLoadStart } = this.props
+     this.setState({ status: STATUS_LOADING })
+     if (onLoadStart) onLoadStart()
+   },
+
+   componentDidMount () {
+     if (this.state.status === STATUS_PENDING) {
+       this._createImageLoader()
+     }
+   },
+
+   componentDidUpdate () {
+    if (this.state.status === STATUS_PENDING && !this.image) {
+      this._createImageLoader()
+    }
+   },
+
+   componentWillReceiveProps(nextProps) {
+    const nextUri = resolveAssetSource(nextProps.source)
+    if (resolveAssetSource(this.props.source) !== nextUri) {
+      this.setState({
+        status: nextUri ? STATUS_PENDING : STATUS_IDLE
+      })
+    }
+  },
+
+  componentWillUnmount() {
+    this._destroyImageLoader()
   }
+
 });
 
 module.exports = Image;
